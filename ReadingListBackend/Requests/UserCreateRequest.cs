8ed9a98c0cc48ddb90429e4ec9b31d78d8d7e7a3
Alex <alexsmith11nd@gmail.com>
2024-01-20 02:47:47 +0000

    Created user req obj

---
+++ ReadingListBackend/Requests/UserCreateRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ReadingListBackend.Requests
+{
+    public class UserCreateRequest
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
