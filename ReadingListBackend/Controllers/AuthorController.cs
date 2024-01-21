55a9445465448190727aff399d6b2643bce15513
Alex <alexsmith11nd@gmail.com>
2024-01-21 01:36:47 +0000

    added controller and model boilerplate for authors

---
+++ ReadingListBackend/Controllers/AuthorController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReadingListBackend.Database;
+using ReadingListBackend.Models;
+
+namespace ReadingListBackend.Controllers
+{
+    public class AuthorController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AuthorController(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Author>>> Get()
+        {
+            return await _context.Authors.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Author>> Get(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+            if (author == null) return NotFound();
+
+            return author;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Author>> Create(Author author)
+        {
+            _context.Authors.Add(author);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new { id = author.Id }, author);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Author author)
+        {
+            if (id != author.Id) return BadRequest();
+
+            _context.Entry(author).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AuthorExists(id)) return NotFound();
+                else throw;
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+            if (author == null) return NotFound();
+
+            _context.Authors.Remove(author);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AuthorExists(int id)
+        {
+            return _context.Authors.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ReadingListBackend/Requests/AuthorCreateRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReadingListBackend.Requests
+{
+    public class AuthorCreateRequest
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Invalid Age")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
+        public string Country { get; set; }
+    }
+}
