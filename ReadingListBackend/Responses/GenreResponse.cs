6c8154b7a3a71fb5f04092978c78f6663df64b62
Alex <alexsmith11nd@gmail.com>
2024-02-02 19:33:49 +0000

    added automapper packages

--- ReadingListBackend/Responses/GenreResponse.cs
+++ ReadingListBackend/Responses/GenreResponse.cs
@@ -1,7 +1,11 @@
-﻿namespace ReadingListBackend.Responses
+﻿using System.Collections.Generic;
+
+namespace ReadingListBackend.Responses
 {
     public class GenreResponse
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<BookResponse> Books { get; set; } = new List<BookResponse>();
     }
 }
