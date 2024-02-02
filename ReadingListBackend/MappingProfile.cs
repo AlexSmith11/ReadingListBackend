2a8826913ceb54c92ba6d5f8be52f1f235948c73
Alex <alexsmith11nd@gmail.com>
2024-02-02 19:41:06 +0000

    Setup automapper dep injection

---
+++ ReadingListBackend/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ReadingListBackend.Models;
+using ReadingListBackend.Responses;
+
+namespace ReadingListBackend
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserResponse>();
+            CreateMap<Author, AuthorResponse>();
+            CreateMap<Genre, GenreResponse>();
+            CreateMap<Book, BookResponse>();
+            CreateMap<List, ListResponse>();
+        }
+    }
+}
--- ReadingListBackend/Program.cs
+++ ReadingListBackend/Program.cs
@@ -1,8 +1,10 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ReadingListBackend;
 using ReadingListBackend.Database;
 using ReadingListBackend.Services;
 
@@ -13,14 +15,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ListService>(); 
+builder.Services.AddScoped<ListService>();
+
+// automapper config
+var mapperConfig = new MapperConfiguration(mc => { mc.AddProfile(new MappingProfile()); });
+IMapper mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddMvc();
 
 // configure DB connection
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 var app = builder.Build();
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
